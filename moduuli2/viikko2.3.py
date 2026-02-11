0fc38c19a19221b839567b1c688f04d9125fbcae
Käyttäjä <edla.vensu@metropolia.fi>
2026-02-11 13:50:16 +0200

    viikko2teht

--- moduuli2/viikko2.3.py
+++ moduuli2/viikko2.3.py
@@ -2,7 +2,7 @@ kanta = float(input("Anna kanta: "))
 korkeus = float(input("Anna korkeus: "))
 
 piiri = 2 * (kanta + korkeus)
-pinta_ala = (kanta + korkeus)
+pinta_ala = (kanta * korkeus)
 
 print("Piiri:", piiri)
 print("Pinta-ala:", pinta_ala)
--- moduuli2/viikko2.5.py
+++ moduuli2/viikko2.5.py
@@ -2,11 +2,15 @@ leiviskat = float(input("Anna leiviskät: "))
 naulat = float(input("anna naulat: "))
 luodit = float(input("anna luodit: "))
 
-naulat_yht = leiviskat * 20 + naulat
-luodit_yht = leiviskat * 32 + luodit
-grammat = luodit_yht * 13.3
+#muunnokset
 
-kg = int(grammat // 1000)
-g = int(grammat % 1000)
+naulat_yht = 32 * luodit_yht
+luodit_yht = 13.3
+leiviskat_yht = 20 * naulat_yht
 
-print(f"Massa: {kg} kiloa ja {g} grammaa")
+massa_grammoina = (leiviskat * leiviskat_yht + naulat * naulat_yht + luodit * luodit_yht)
+kilot = int(massa_grammoina// 1000)
+grammat = (massa_grammoina % 1000)
+
+print(f"{kilot} kilogrammaa ja {g} grammaa")
+
