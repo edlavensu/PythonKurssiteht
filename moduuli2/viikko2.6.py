807a0becc0261e7bd9837a73d99c7bcf4fa6f1d3
Käyttäjä <edla.vensu@metropolia.fi>
2026-02-11 13:58:22 +0200

    viikko2teht

--- moduuli2/viikko2.6.py
+++ moduuli2/viikko2.6.py
@@ -3,4 +3,10 @@ num1 = random.randint(0, 9)
 num2 = random.randint(0, 9)
 num3 = random.randint(0, 9)
 
+num4 = random.randint(1, 6)
+num5 = random.randint(1, 6)
+num6 = random.randint(1, 6)
+num7 = random.randint(1, 6)
+
 print("Numerolukon koodi on: ", num1, num2, num3)
+print("Numerolukon koodi on: ", num4, num5, num6)
