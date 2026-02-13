7be340fff7a85ffb906ff07dd1da16d6889fdc8d
Käyttäjä <edla.vensu@metropolia.fi>
2026-02-13 17:38:19 +0200

    viikko3teht

---
+++ moduuli3/viikko3.2.py
@@ -0,0 +1,13 @@
+hyttiluokka = input("Anna hyttiluokka (Lux, A, B, C): ")
+
+if hyttiluokka == "Lux":
+    print("hytti yläkannella ja parveke")
+elif hyttiluokka == "A":
+    print("ikkunallinen hytti autokannen yläpuolella")
+elif hyttiluokka == "B":
+    print("Ikkunaton hytti autokannen yläpuolella")
+elif hyttiluokka == "C":
+    print("Ikkunaton hytti autokannen alapuolella")
+else:
+    print("Virheellinen hyttiluokka")
+
