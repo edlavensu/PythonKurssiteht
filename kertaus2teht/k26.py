b62e98e71b96f10a8d1144858cd0cfd1d146dc3d
Käyttäjä <edla.vensu@metropolia.fi>
2026-03-26 20:21:40 +0200

    kertaus2

--- kertaus2teht/k26.py
+++ kertaus2teht/k26.py
@@ -10,10 +10,39 @@ def tulo(a, b):
 def jako(a, b):
     return a / b
 
-a = int(input("Anna ensimmäinen luku: "))
-b = int(input("Anna toinen luku: "))
 
-print("Summa:", summa(a, b))
-print("Erotus:", erotus(a, b))
-print("Tulo:", tulo(a, b))
-print("Jako:", jako(a, b))
+print("käytä laskinta!")
+
+while True:
+    print("\nValitse mitä toimintoa haluat käyttää:")
+    print(" A: Yhteenlasku")
+    print(" B: Vähennyslasku")
+    print(" C: Kertolasku")
+    print(" D: Jakolasku")
+
+    valinta = input("Valintasi (A-D, Q lopettaa): ")
+
+    if valinta == "Q":
+        print("Hei hei!")
+        break
+
+    if valinta in ["A", "B", "C", "D"]:
+        a = float(input("Anna ensimmäinen luku: "))
+        b = float(input("Anna toinen luku: "))
+
+        if valinta == "A":
+            print("Tulos:", summa(a, b))
+
+        elif valinta == "B":
+            print("Tulos:", erotus(a, b))
+
+        elif valinta == "C":
+            print("Tulos:", tulo(a, b))
+
+        elif valinta == "D":
+            if b == 0:
+                print("Ei voi jakaa nollalla!")
+            else:
+                print("Tulos:", jako(a, b))
+    else:
+        print("Virheellinen valinta!")
